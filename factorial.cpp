f31c01243a86bf193f3d937ea42a9cd52c5ad054
ombhombe2344 <ombhombe15@gmail.com>
2024-09-27 13:08:32 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,24 @@
+//Name:Om Bhombe
+//PRN:23070123039
+//Aim:Implemention of Recursion to find Factorial of given Number
+
+#include<iostream>
+using namespace std;
+int factorial(int n) {
+    if (n <= 1)
+        return 1;
+    else 
+        return (n * factorial(n - 1));
+}
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    return 0;
+}
+
+
+/*Output:
+Enter a number: 5
+Factorial of 5 is 120*/
